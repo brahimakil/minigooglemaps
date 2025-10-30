0d10dc199521ace9bf6543d26988e5eff2a67459
ibrahim akil <brhimakil.1@gmail.com>
2025-10-30 15:40:01 +0200

    fixing the error of maximum callstack

--- next.config.js
+++ next.config.js
@@ -9,30 +9,17 @@ const nextConfig = {
     unoptimized: process.env.NODE_ENV === 'production',
   },
   staticPageGenerationTimeout: 300,
-  exclude: [/\/api\/update-doc/],
   experimental: {
     serverComponentsExternalPackages: ['firebase'],
-    esmExternals: false,
     optimizePackageImports: [
       '@firebase/firestore',
       '@firebase/auth',
       '@firebase/storage',
       'chart.js'
     ],
-    serverActions: true,
-    logging: 'verbose',
-    modularizeImports: {
-      'chart.js/auto': {
-        transform: 'chart.js/auto'
-      }
-    }
+    serverActions: true
   },
-  logLevel: 'DEBUG',
   webpack: (config) => {
-    config.infrastructureLogging = { 
-      level: 'verbose', 
-      debug: /micromatch/ 
-    };
     config.optimization.moduleIds = 'named';
     config.optimization.chunkIds = 'named';
     return config;
